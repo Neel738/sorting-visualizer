3197fdeb5980e44979ddccb7787be016a96d00a7
Neel <Neel738@hotmail.com>
2021-07-15 16:40:47 -0400

    basic array generation - no styling yet

--- src/components/ArrayVisualizer.js
+++ src/components/ArrayVisualizer.js
@@ -1,22 +1,42 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from "react";
 
 const ArrayVisualizer = () => {
-    
-    const [array, setArray] = useState([]);
+  const [array, setArray] = useState([]);
+ 
 
-    const generateArray = (upperbound) => {
 
+  const generateArray = () => {
+    let new_array = [];
+
+    for (let i = 0 ; i < 50; i++) {
+        new_array.push(getRandomNumberBetween(5, 200));
     }
+   
 
-    return <div className='array-container'>
-        This is an array
-      
+    setArray(new_array);
+  };
+
+  
+
+  return (
+    <div className="array-container">
+      <div className="playground-controls">
+        <button onClick={generateArray}>Generate New Array</button>
+      </div>
+
+      <div className="array-content">
+        
+        {array.map((val, indx) => (
+          <div className={'array-value'} key={indx} style={{height: `${val}px`}}></div>
+        ))}
+      </div>
     </div>
-}
-export default  ArrayVisualizer
-
+  );
+};
+export default ArrayVisualizer;
 
 // Copied from w3 schools: https://www.w3schools.com/jsref/jsref_random.asp
 function getRandomNumberBetween(lower, upper) {
-    Math.floor((Math.random() * upper) + lower);
+    var new_num = Math.floor(Math.random() * upper + lower);
+  return new_num;
 }
--- src/components/Playground.js
+++ src/components/Playground.js
@@ -1,27 +1,16 @@
-import React from 'react';
-import ArrayVisualizer from './ArrayVisualizer'
+import React from "react";
+import ArrayVisualizer from "./ArrayVisualizer";
 
 const Playground = () => {
+  
 
-
-
-
-
-    return (
-
-        <div>
- <div className="playground-container">
-        <ArrayVisualizer></ArrayVisualizer>
-           </div> 
-
-           <div className='playground-controls'>
-                <button>Generate New Array</button>
-           </div>
-        </div>
-       
-    );
-}
-
-
+  return (
+    <div>
+      <div className="playground-container">
+        <ArrayVisualizer />
+      </div>
+    </div>
+  );
+};
 
 export default Playground;
