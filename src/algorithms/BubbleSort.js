557b061cb897dd573e289bb52799696fb9b8894b
Neel <Neel738@hotmail.com>
2021-07-15 23:28:44 -0400

    faulty visualizer happeing

---
+++ src/algorithms/BubbleSort.js
@@ -0,0 +1,29 @@
+function BubbleSort(array) {
+    // Write your code here.
+    let animations = [];
+
+      for (let i  =0; i < array.length; i++) {
+          for (let j=0; j < (array.length - i - 1); j++) {
+              if (array[j] > array[j+1]) {
+                  array = swap(array, j, j+1);
+                  animations.push([j, j+1])
+              }
+          }
+      }
+  
+      return animations;
+  }
+  
+  function swap(array, i , j) {
+      
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  
+      return array;
+  
+  }
+
+
+
+export default BubbleSort;
--- src/components/Visualizer.js
+++ src/components/Visualizer.js
@@ -1,46 +1,79 @@
 import React, { useState, useEffect } from "react";
-
+import BubbleSort from "../algorithms/BubbleSort";
 const Visualizer = () => {
   const [array, setArray] = useState([]);
+  const [currentSwap, setCurrentSwap] = useState([]);
+  const [counter, setCounter] = useState(0);
 
   const generateArray = () => {
     let new_array = [];
 
-    for (let i = 0 ; i < 150; i++) {
-        new_array.push(getRandomNumberBetween(1, 400 ));
+    for (let i = 0; i < 150; i++) {
+      new_array.push(getRandomNumberBetween(1, 400));
     }
-   
-    setArray(new_array)
+
+    setArray(new_array);
   };
+
+  const doSort = () => {
+    let animations = BubbleSort(array);
+    
+    
+
+    for (let i = 0; i < animations.length; i++) {
+      
+     
+
+     
+
+        let next_switch = animations[i];
+        console.log(`switching ${next_switch[0]} and ${next_switch[1]}`)
+        let bars = document.getElementsByClassName('array-value');
+
+
+        setTimeout(() => {
+        const [barOneIdx, height] = animations[i];
+          const barOneStyle = bars[barOneIdx].style;
+          barOneStyle.height = `${height}px`;
+
+      }, i * 10)
+      
+    }
+    
+  };
+
+  function swap(i, j) {
+    
+    let new_array = array;
+
+    let temp = new_array[i];
+    new_array[i] = new_array[j];
+    new_array[j] = temp;
+
+    setArray(new_array);
+  }
 
   useEffect(generateArray, []);
 
   return (
     <div className="container visualizer-container">
-
-
-        
-        <center>
-
+      <center>
+        {currentSwap}
         <div className="playground-controls">
-        
-        <button onClick={generateArray}>Generate New Array</button>
-        <button>Sort</button>
-      
-    </div>
-    <center>
-    <div className="array-container">
-        {array.map((val, indx) => (
-          <div
-            className={"array-value"}
-            key={indx}
-            style={{ height: `${val}px` }}
-          ></div>
-        ))}
-      </div>
-    </center>
-     
-      
+          <button onClick={generateArray}>Generate New Array</button>
+          <button onClick={doSort}>Sort</button>
+        </div>
+        <center>
+          <div className="array-container">
+            {array.map((val, indx) => (
+              <div
+                className={"array-value"}
+                key={indx}
+                style={{ height: `${val}px` }}
+              ></div>
+            ))}
+          </div>
+        </center>
       </center>
     </div>
   );
@@ -48,10 +81,8 @@ const Visualizer = () => {
 
 export default Visualizer;
 
-
-
 // Copied from w3 schools: https://www.w3schools.com/jsref/jsref_random.asp
 function getRandomNumberBetween(lower, upper) {
-    var new_num = Math.floor(Math.random() * upper + lower);
+  var new_num = Math.floor(Math.random() * upper + lower);
   return new_num;
 }
